750141c69307ad963198cd2b814978a6ad21369c
kamakospan <kamazhayokama@gmail.com>
2023-04-11 01:40:10 +0600

    adding changes to lab 8

---
+++ lab 8/racer/1.py
@@ -0,0 +1,160 @@
+import pygame as pg
+from pygame import mixer as mix
+from sys import exit
+import random, time
+
+pg.init()
+mix.init()
+clock=pg.time.Clock()
+
+#CONSTANTS
+SCR_W=768
+SCR_H=594
+ENEMYSPEED=3
+JAKESPEED=5
+SCORECOINS=0
+SCORE=0
+JAKEMODE=False
+
+f1 = pg.font.Font('Gemstone.ttf', 20)
+f2 = pg.font.Font('Gemstone.ttf', 36)
+
+screen=pg.display.set_mode((SCR_W, SCR_H))
+pg.display.set_caption("racer")
+
+mix.music.load("Subway Surfers OST Extended (256  kbps).mp3")
+mix.music.set_volume(0.2)
+mix.music.play(-1)
+
+bg=pg.image.load("background.png")
+bg1=pg.image.load("gameover.png")
+
+
+deadscreen=False
+
+class Enemy(pg.sprite.Sprite):
+      def __init__(self):
+        super().__init__() 
+        self.image = pg.image.load("enemy1.png")
+        self.rect = self.image.get_rect()
+        self.rect.center=(random.randint(40,SCR_W-40),0) 
+ 
+      def move(self):
+        self.rect.move_ip(0,ENEMYSPEED)
+        if (self.rect.bottom > 650):
+            global SCORE
+            SCORE+=5
+            self.rect.top = 0
+            self.rect.center = (random.randint(30, 370), 0)
+ 
+      def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+
+class Player(pg.sprite.Sprite):
+    def __init__(self):
+        super().__init__() 
+        self.image = pg.image.load("jakeorig.png")
+        self.imagecool=pg.image.load("jakehappy.png")
+        self.imagefail=pg.image.load("jakedead.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (160, 520)
+    def move(self):
+        pressed_keys = pg.key.get_pressed()
+        if self.rect.left > 0:
+              if pressed_keys[pg.K_LEFT]:
+                  self.rect.move_ip(-1*(JAKESPEED+SCORECOINS//10), 0)
+        if self.rect.right < SCR_W:        
+              if pressed_keys[pg.K_RIGHT]:
+                  self.rect.move_ip(JAKESPEED+SCORECOINS//10, 0)
+    def draw(self, surface):
+        if JAKEMODE:
+            surface.blit(self.imagecool, self.rect)
+        else:
+            surface.blit(self.image, self.rect)  
+
+CAPY=Player()
+
+class Coins(pg.sprite.Sprite):
+    def __init__(self):
+        super().__init__() 
+        self.image = pg.image.load("coin.png")
+        self.rect = self.image.get_rect()
+        mylist1=[*range(20, max(CAPY.rect.center[0]-50,20))]
+        mylist2=[*range(min(CAPY.rect.center[0]+50, SCR_W-20), SCR_W-20)]
+        mylist1.extend(mylist2)
+        self.rect.center=(int(random.choice(mylist1)),525) 
+    def update(self):
+        mylist1=[*range(20, max(CAPY.rect.center[0]-50,20))]
+        mylist2=[*range(min(CAPY.rect.center[0]+50, SCR_W-20), SCR_W-20)]
+        mylist1.extend(mylist2)
+        self.rect.center=(int(random.choice(mylist1)),525)
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)  
+ 
+
+
+E1=Enemy()
+ORANGE=Coins()
+
+enemies = pg.sprite.Group()
+enemies.add(E1)
+
+regards=pg.sprite.Group()
+regards.add(ORANGE)
+
+all_sprites = pg.sprite.Group()
+all_sprites.add(CAPY)
+all_sprites.add(E1)
+
+#Adding a new User event 
+INC_SPEED = pg.USEREVENT + 1
+pg.time.set_timer(INC_SPEED, 10000)
+
+while True:
+    for event in pg.event.get():
+        if event.type==pg.QUIT:
+            pg.quit()
+            exit()
+        if event.type==INC_SPEED:
+            ENEMYSPEED+=1
+    text1 = f1.render("x{}".format(str(SCORE)),False,(255, 255, 255))
+    text2 = f1.render("x{}".format(str(SCORECOINS)),False,(255, 255, 255))
+    if deadscreen:
+        screen.blit(bg1,(0,0))
+        tex1=f2.render("TOTAL SCORE:{}".format(str(SCORE)),True,(255, 255, 255))
+        rect=tex1.get_rect()
+        rect.center=(200,480)
+        tex2=f2.render("COIN:{}".format(str(SCORECOINS)),True,(255, 255, 255))
+        rect2=tex2.get_rect()
+        rect2.center=(200,530)
+        screen.blit(tex1, rect)
+        screen.blit(tex2,rect2)
+    else:
+        screen.blit(bg, (0,0))
+        screen.blit(ORANGE.image, ORANGE.rect)
+        for entity in all_sprites:
+            screen.blit(entity.image, entity.rect)
+            entity.move()
+        screen.blit(text1, (7,35))
+        screen.blit(text2, (38,7))
+
+    if pg.sprite.spritecollideany(CAPY, regards):
+          ORANGE.update()
+          SCORECOINS+=1
+          JAKEMODE=1
+ 
+    #To be run if collision occurs between Player and Enemy
+    if pg.sprite.spritecollideany(CAPY, enemies):
+          pg.display.update()
+          mix.music.stop()
+          mix.music.load("gameover.mp3")
+          mix.music.set_volume(0.3)
+          mix.music.play(-1)
+          for entity in all_sprites:
+                entity.kill() 
+          deadscreen=True
+
+    
+    pg.display.update()
+    clock.tick(60)
